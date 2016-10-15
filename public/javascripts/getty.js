cdd56c916f0c1ba6606ce76f290d51afd72907b1
ravenusmc <mcuddy77@gmail.com>
2016-10-15 07:32:36 -0400

    added d3 in

--- public/javascripts/getty.js
+++ public/javascripts/getty.js
@@ -15,7 +15,7 @@ function getWords(){
   tempwords = wordsOne.concat(wordsTwo);
   allWords = tempwords.concat(wordsThree);
   for (var i = 0; i < allWords.length; i++){
-    console.log(allWords[i]);
+    // console.log(allWords[i]);
   }
 
 }
@@ -24,12 +24,38 @@ window.onload = function() {
   getWords();
 };
 
-I have to remember how to find the particular value which repeats in an array. 
-1. Get one word, start at index = 0, search through the entire array. 
-2. 
+d3.select(".test").append("p").text("New Paragraph! YAY IT WORKS");
 
-for loop - selects particular word 
-  for loop will go through comparing selected word to everything else. 
+// I have to remember how to find the particular value which repeats in an array. 
+// 1. Get one word, start at index = 0, search through the entire array. 
+// 2. 
+
+// for loop - selects particular word 
+//   for loop will go through comparing selected word to everything else. 
+
+// string = input("Please enter in a word: ")
+// mylist_char = []
+// mylist_count = []
+// char = None
+// count = 0
+
+// for ch in string:
+//     print("ch:",ch)
+//     temp_char = ch
+//     temp_count = 0
+//     for ch1 in string:
+//         print("ch1:",ch1)
+//         if temp_char == ch1:
+//             temp_count += 1
+
+//     if temp_count > count:
+//         count = temp_count
+//         char = temp_char
+
+// mylist_char.append(char)
+// mylist_count.append(count)
+
+// print(mylist_char)
 
 
 // phrase = input("Please enter in a word: ")
