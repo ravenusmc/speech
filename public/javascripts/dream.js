0750a0f2038117de022ba63ce462cb1c40bdf5dd
ravenusmc <mcuddy77@gmail.com>
2016-12-02 15:03:47 -0500

    wrote code to pull all words from dream speech

--- public/javascripts/dream.js
+++ public/javascripts/dream.js
@@ -12,27 +12,20 @@ function main(){
 
 }
 
-//This function grabs all of the words in the speech and places them into an array. 
+//This function grabs all of the words in the speech and places them into an array
 function getWords(){
 
-  //Creating arrays to hold all of the words for each paragraph.
+  //Creating an array to hold all of the words for each paragraph
   var wordsOne = [];
-  var wordsTwo = [];
-  var tempWords = [];
 
-  //Pulling the words from each paragraph. 
+  //Pulling the words from each paragraph
   var one = document.getElementById('wordsOne').innerHTML;
-  var two = document.getElementById('wordsTwo').innerHTML;
 
-  //Spliting the words up and then placing them into an array. 
+  //Spliting the words up and then placing them into an array
   wordsOne = one.split(" ");
-  wordsTwo = two.split(" ");
 
-  //Combining all of the arrays
-  tempWords = wordsOne.concat(wordsTwo);
-  //allWordsTemp = tempwords.concat(wordsThree);
-
-  return tempWords; 
+  //Returning the array
+  return wordsOne; 
 
 }
 
