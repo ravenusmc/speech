e97db2918a5e73bb514bc8f00897daca7915ea3c
ravenusmc <mcuddy77@gmail.com>
2016-12-02 13:20:14 -0500

    working on dream speech page

--- public/javascripts/dream.js
+++ public/javascripts/dream.js
@@ -1,17 +1,2 @@
-var nums = [2,3,4,5,',', 5,6,','];
-var allnums = [];
-
-
-for (var i = 0; i < nums.length; i++){
-  if (nums[i] != ','){
-    allnums.push(nums[i]);
-  }
-}
-
-for (var i = 0; i < allnums.length; i++){
-
-
-}
-
-console.log(allnums);
+alert("Hello");
 
