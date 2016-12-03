e8c865e356feb1dd2fabc6620f71315abffda71b
ravenusmc <mcuddy77@gmail.com>
2016-12-03 15:53:43 -0500

    working on sorting words

--- public/javascripts/dream.js
+++ public/javascripts/dream.js
@@ -16,7 +16,8 @@ function main(){
 function getWords(){
 
   //Creating an array to hold all of the words for each paragraph
-  var wordsOne = [];
+  var allWords = [];
+  var words = [];
 
   //Pulling the words from each paragraph
   var one = document.getElementById('wordsOne').innerHTML;
@@ -24,8 +25,22 @@ function getWords(){
   //Spliting the words up and then placing them into an array
   wordsOne = one.split(" ");
 
+  //In this for loop I am removing all words that I do not care about such as coordinating conjunctions etc. 
+  for (var i = 0; i < allWords.length; i++){
+    if (allWords[i] != "and" && allWords[i] != "on" && allWords[i] != "of" && allWords[i] != "for"
+    && allWords[i] != "to" && allWords[i] != "or" && allWords[i] != "a" && allWords[i] != "that" && allWords[i] != "in"
+    && allWords[i] != "do" && allWords[i] != "so" && allWords[i] != "this" && allWords[i] != "the" && allWords[i] != "The"
+    && allWords[i] != "any" && allWords[i] != "can" && allWords[i] != "as" && allWords[i] != "But"
+    && allWords[i] != "should" && allWords[i] != "from" && allWords[i] != "nor" && allWords[i] != "what"
+    && allWords[i] != "but" && allWords[i] != "thus" && allWords[i] != "which" && allWords[i] != "have"){
+      words.push(allWords[i]);
+    }
+  }
+
+
+
   //Returning the array
-  return wordsOne; 
+  return words; 
 
 }
 
