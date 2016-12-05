9574593c61126ef5a556ff4eaaf2888d46846bd8
ravenusmc <mcuddy77@gmail.com>
2016-12-05 15:25:04 -0500

    Added d3 code to dream.js file

--- public/javascripts/dream.js
+++ public/javascripts/dream.js
@@ -3,14 +3,54 @@ function main(){
 
   //This array will hold all of the words. 
   var allwords = [];
+  var words = [];
+  var w = 1000;
+  var h = 6000;
+  var barPadding = 1;
 
   //Getting the array that will hold all of the words filled. 
   allWords = getWords();
+  //This function will return an array of objects that will hold all of the words and the frequency that it appears.
   words = getwordCount(allWords);
 
   //Displaying all the words for testing purposes
-  console.log(allWords);
+  //console.log(allWords);
+  console.log(words);
 
+  //D3 code 
+  var svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
+  svg.selectAll("rect")
+    .data(words)
+    .enter()
+    .append("rect")
+    .attr("width", function(d){
+      return d.count * 60;
+    })
+    .attr("height", 100)
+    .attr("y", function(d, i)
+    {
+      return i * 20;
+    })
+    .attr("fill", "red");
+
+
+  svg.selectAll("text")
+    .data(words)
+    .enter()
+    .append("text")
+    .attr("fill", "#ffffff")
+    //This moves the text up and down
+    .attr("y", function(d, i){
+      return i * 80 + 30;
+    })
+    //This moves the placement of the text left and right
+    .attr("x", 20)
+    .text(function(d){
+      return  "word: " + d.word + " Word Count: " + d.count;
+    })
 
 }
 
@@ -47,7 +87,25 @@ function getWords(){
 }
 
 //This function will find the count of each word in the allwords array.
-function 
+function getwordCount(allWords){
+
+  var words = []
+
+  for (var i = 1; i < allWords.length; i++){
+      var tempWord = allWords[i];
+      var wordCount = 0
+      for (var j = 1; j < allWords.length; j++){
+        if (tempWord == allWords[j]){
+          wordCount += 1;
+        }
+      }
+      if (wordCount <= 8 &&  wordCount >= 2){
+        words.push({word: tempWord, count: wordCount });
+      }     
+    }
+
+  return words;
+}
 
 
 //This is what will execute the code once the screen has loaded. 
